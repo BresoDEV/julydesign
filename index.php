487cb8521723e458971eea0a368dd6bed7ff8895
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2021-01-22 02:33:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <title>Meu Redirect</title>
-    <meta http-equiv="refresh" content="1; URL='https://bresodev.github.io/julydesign/marketingdigital'"/>
+    <meta http-equiv="refresh" content="1; URL='https://bresodev.github.io/julydesign/marketingdigital/index.html'"/>
 </head>
 <body>
 ...
